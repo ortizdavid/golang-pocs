273c8781c179092fcfa213fd7a5f5d737360d693
eduardodecarvalho <decarvalho.dev@gmail.com>
2025-01-03 12:41:35 +0100

    add go gRPC project as a PoC

---
+++ go-grpc/cmd/server/server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"golang-pocs/pb"
+
+	"golang-pocs/services"
+	"google.golang.org/grpc"
+)
+
+func main() {
+
+	lis, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterUserServiceServer(grpcServer, services.NewUserService())
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+}
---
+++ go-grpc/go.mod
@@ -0,0 +1,16 @@
+module golang-pocs
+
+go 1.22
+
+require (
+	google.golang.org/grpc v1.69.2
+	google.golang.org/protobuf v1.36.1
+)
+
+require (
+	github.com/golang/protobuf v1.5.4 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+)
