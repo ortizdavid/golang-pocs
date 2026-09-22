996fd7a8cba723f22d51b8e4c4d62760fb6f9c21
Ortiz David <70776057+ortizdavid@users.noreply.github.com>
2026-09-22 21:26:04 +0000

    Implement auto-healing functionality with context

--- resilience/resources/auto_healing.go
+++ resilience/resources/auto_healing.go
@@ -1,13 +1,46 @@
 package resources
 
-func (res *InfraResources) StartAutohealing() {
-	
+import (
+	"context"
+	"sync"
+	"time"
+)
+
+type AutoHealing struct {
+	res *InfraResources
 }
 
-func (res *InfraResources) RecoverCache() {
-	res.mu.Lock()
+func NewAutoHealing(res *InfraResources) *AutoHealing {
+	return &AutoHealing{res: res}
 }
 
-func (res *InfraResources) RecoverDatabase() {
-	res.mu.Lock()
+func (ah *AutoHealing) StartAutohealing(ctx context.Context) {
+	ticker := time.NewTicker(10 * time.Second)
+
+	go func() {
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				go ah.RecoverDatabase()
+				go ah.RecoverCache()
+			}
+		}
+	}()
+}
+
+func (ah *AutoHealing) RecoverCache() {
+	ah.res.mu.Lock()
+	defer ah.res.mu.Unlock()
+
+	// Lógica de verificação e reconexão do Cache
+}
+
+func (ah *AutoHealing) RecoverDatabase() {
+	ah.res.mu.Lock()
+	defer ah.res.mu.Unlock()
+
+	// Lógica de verificação e reconexão do Database
 }
